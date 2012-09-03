4396349b1fa77b7e65e8af89d9e3a27c8ec1e5bf
Komachi Shuhei <b1010049@fun.ac.jp>
2012-09-03 13:49:04 +0900

    test2

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,67 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
+//windowを作る
+	
+	var win1 = Ti.UI.createWindow({
+	backgroundColor: 'blue'	
+	}); 
 
+	var win2 = Ti.UI.createWindow({
+	backgroundColor: 'red'	
+	}); 
+//viewを作る
 
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
-});
+	var view1 = Ti.UI.createView();
+	var view2 = Ti.UI.createView();
+//labelを作る
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
+	var label = Ti.UI.createLabel({
+		
+		text: 'Hello World!',
+		height: 30,
+		weight: 100,
+		top: 100,
+		color: 'yellow'
+	});
 
-win1.add(label1);
+	var map = Ti.Map.createView({
+		mapType: Ti.Map.STANDARD_TYPE,
+		region: {latitude:35.658517, longitude:139.701334, latitudeDelta:0.01, longitudeDelta:0.01},
+    	animate: true,
+    	regionFit: true,
+    	width: 300,
+    	height: 400
+	});
+ 
+	
+	var button1 = Ti.UI.createButton({
+		title: 'push',
+		top: 200,
+		height: 30,
+		weight: 100
+	});
 
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+	button1.addEventListener('click', function(e){
+    win2.open();
 });
 
-win2.add(label2);
+	var button2 = Ti.UI.createButton({
+		title: 'push',
+		top: 50,
+		height: 30,
+		weight: 100
+	});
 
+	button2.addEventListener('click', function(e){
+    win2.close();
+	});
 
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+//viewにlabelとbuttonを追加
+	view1.add(label);
+	view1.add(button1);
+	view2.add(map);
+	view2.add(button2)
+//windowにviewを追加	
+	win1.add(view1);
+	win2.add(view2);
+//windowを開く	
+	win1.open();
